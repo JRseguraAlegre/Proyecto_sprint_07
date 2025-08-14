02a55e35cbeafede2c8e679152c408dca8bb22f9
JRseguraAlegre <joaquin25segura@gmail.com>
2025-08-14 14:26:02 -0500

    actualizaciones mas recientes

--- Notebook app.py
+++ Notebook app.py
@@ -202,3 +202,27 @@ st.download_button(
     mime="text/csv",
 )
 
+car_data = pd.read_csv('vehicles_us.csv')
+hist_button = st.button("Construir histograma")
+
+if hist_button:
+    st.write("Creación de un histograma para el conjunto de datos")
+
+    fig = px.histogram(car_data, x="odometer", nbins=40,
+                       title="Distribución de kilometraje")
+    st.plotly_chart(fig, use_container_width=True)
+
+# =======================
+# Botón para gráfico de dispersión
+# =======================
+scatter_button = st.button("Construir gráfico de dispersión")
+
+if scatter_button:
+    st.write("Creación de un gráfico de dispersión")
+
+    fig2 = px.scatter(car_data,
+                      x="odometer",
+                      y="price",
+                      color="condition",
+                      title="Precio vs Kilometraje")
+    st.plotly_chart(fig2, use_container_width=True)
